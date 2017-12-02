4f960fa7a922fb8ae0880229693b516b5d6d7891
Martin Sandström <martin@marteinn.se>
2017-12-02 23:37:15 +0100

    Add destillery to elixir app

--- src/mix.exs
+++ src/mix.exs
@@ -20,7 +20,7 @@ defmodule ExampleApp.Mixfile do
   def application do
     [
       mod: {ExampleApp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :distillery]
     ]
   end
 
@@ -40,7 +40,8 @@ defmodule ExampleApp.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:distillery, "~> 1.5.2"}
     ]
   end
 
